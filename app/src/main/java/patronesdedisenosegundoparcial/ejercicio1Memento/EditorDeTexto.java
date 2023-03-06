135deac3b196697dc37d73c1b143509db86fad12
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-06 14:49:12 -0400

    Create EditorDeTexto.java

---
+++ app/src/main/java/patronesdedisenosegundoparcial/ejercicio1Memento/EditorDeTexto.java
@@ -0,0 +1,20 @@
+package patronesdedisenosegundoparcial.ejercicio1Memento;
+
+public class EditorDeTexto {
+    private Tesis tesis;
+
+    public void setTesis(Tesis tesis){
+        System.out.println("TESIS LISTA PARA SER GUARDADA...");
+        this.tesis = tesis.clone();
+    }
+
+    public Memento createMemento(String fecha){
+        return new Memento(tesis.clone(), fecha);
+    }
+
+    public Tesis recuperarTesis(Memento memento){
+        this.tesis = memento.getTesis();
+        System.out.println("SE RECUPERO LA TESIS DE: "+memento.getFecha()+"--------------------------------------------------------------");
+        return this.tesis;
+    }
+}
