d51d36443c069f34199fea8787c059f853707890
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-06 15:56:49 -0400

    Create Manager.java

---
+++ app/src/main/java/patronesdedisenosegundoparcial/ejercicio6/Manager.java
@@ -0,0 +1,13 @@
+package patronesdedisenosegundoparcial.ejercicio6;
+
+public class Manager {
+    private IEstructurador estructurador;
+
+    public Manager(IEstructurador estructurador) {
+        this.estructurador = estructurador;
+    }
+
+    public String construirMensaje(Desarrollador desarrollador){
+        return this.estructurador.construirMensaje(desarrollador);
+    }
+}
