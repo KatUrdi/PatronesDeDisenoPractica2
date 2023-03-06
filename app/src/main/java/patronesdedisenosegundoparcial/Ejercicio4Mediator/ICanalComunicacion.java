0c08eafb8d282b1a15dfeea61be4372a5704231f
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-06 15:11:43 -0400

    Create ICanalComunicacion.java

---
+++ app/src/main/java/patronesdedisenosegundoparcial/Ejercicio4Mediator/ICanalComunicacion.java
@@ -0,0 +1,6 @@
+package patronesdedisenosegundoparcial.ejercicio4Mediator;
+
+public interface ICanalComunicacion {
+    void send(String message, Persona persona, Persona recipiente);
+    void send(String message, Persona persona);
+}
