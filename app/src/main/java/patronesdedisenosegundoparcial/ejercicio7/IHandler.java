e2c58111ce72a3f7d6a32fecbf167434e1516e2d
Katzumi Urdininea <77220627+KatUrdi@users.noreply.github.com>
2023-03-07 08:42:54 -0400

    Create IHandler.java

---
+++ app/src/main/java/patronesdedisenosegundoparcial/ejercicio7/IHandler.java
@@ -0,0 +1,7 @@
+package patronesdedisenosegundoparcial.ejercicio7;
+
+public interface IHandler {
+    void setNext(IHandler handler);
+    IHandler next();
+    void pagoPrestamo(Persona persona, double montoAPagar);
+}
